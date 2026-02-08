bfad5c7729413c2f275f2fdb20f87e5b021846d5
Adrian Chiscop <adrianchiscop@users.noreply.github.com>
2026-02-08 21:24:37 +0100

    Polish filter More/Less button alignment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal same-origin service worker for instant repeat loads on mobile WebKit/Android. */
-const CACHE_NAME = 'scp-cache-20260208s';
+const CACHE_NAME = 'scp-cache-20260208t';
 
 const PRECACHE_PATHS = [
   './',
